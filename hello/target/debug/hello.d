01b0abb2fd87d7e8bf154c85cdb660337e718071
byz <xiaobudongzhang@fangdd.com>
2018-01-07 01:40:26 +0800

    add

---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/root/projects/rust/hello/target/debug/hello: /root/projects/rust/hello/src/main.rs
---
+++ hello/target/release/hello.d
@@ -0,0 +1,1 @@
+/root/projects/rust/hello/target/release/hello: /root/projects/rust/hello/src/main.rs
